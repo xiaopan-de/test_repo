07fd11089ab2c762faeaea3dd2a8431ee501299c
Xiao Pan <38347346+xiaopan-de@users.noreply.github.com>
2023-01-31 12:24:10 +0100

    update test

--- gen_html.sh
+++ gen_html.sh
@@ -2,9 +2,10 @@
 
 set -u
 
-mkdir -p subfolder/test
-echo "Hello World" > subfolder/test/index.html
+mkdir -p _site/github-pages/test
+echo "Hello World" > _site/github-pages/test/index.html
 
-ls -l subfolder/test/index.html
+ls -l _site/github-pages/test/index.html
+
 
 exit 0
